fefe7d4a4a951b893142ac0c973dd135fbc52a5a
Guilmour <guilmour@alunos.utfpr.edu.br>
2017-08-23 11:13:00 -0300

    Provisory email login + fb and tw signup option

--- models/User.js
+++ models/User.js
@@ -10,7 +10,7 @@ var schemaOptions = {
 };
 
 var userSchema = new mongoose.Schema({
-  username: {type: String, unique: true},
+   username: {type: String, unique: true},
   email: {type: String, unique: true},
   name: String,
   password: String,
