ce4c873bad5ff074e3341bc24727d201f9b13609
Guilmour <guilmour@alunos.utfpr.edu.br>
2018-02-25 00:14:20 -0300

    Add new cover_img field to the User model.

--- models/User.js
+++ models/User.js
@@ -20,6 +20,7 @@ var userSchema = new mongoose.Schema({
   location: String,
   website: String,
   picture: String,
+  cover_picture: String,
   facebook: String,
   twitter: String,
   mod: Boolean
