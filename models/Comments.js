ec5f5a57f003bb88a3b39fdca0b0d88b7c22240e
Guilmour <guilmour@alunos.utfpr.edu.br>
2018-02-25 23:37:47 -0300

    Comments Model

---
+++ models/Comments.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+var schemaOptions = {
+  timestamps: true,
+  toJSON: {
+    virtuals: true
+  }
+};
+
+var commentSchema = new mongoose.Schema({
+  date: Date,
+  comments: [{
+    body: String,
+    date: Date
+    creator: { type : mongoose.Schema.ObjectId, ref : 'User' }
+  }],
+  attachedToUser: { type : mongoose.Schema.ObjectId, ref : 'User' },
+  attachedToWatch: { type : mongoose.Schema.ObjectId, ref : 'Watch' },
+}, schemaOptions);
+
+
+var Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
