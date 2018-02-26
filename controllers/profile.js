efd10446854293c8b6104024e4c83cfd7dd6bc5b
Guilmour <guilmour@alunos.utfpr.edu.br>
2018-02-25 23:37:37 -0300

    Improvements on the the Profile Page

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,19 +1,36 @@
+'use strict';
+
 var User = require('../models/User');
+var Watch = require('../models/Watch');
 var mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 
-// Get Movie or Series
+/**
+ * Get the profile of someone
+ */
 exports.profileGet = function(req, res){
-  User.findOne({ 'username': req.params.username }, function(err, p){
+  var options = {
+    profile: [],
+    watch: []
+  }
 
-    if (!p) {
+  User.findOne({ 'username': req.params.username }, function(err, p){
+    if(!p){
       return res.redirect('/404');
     }
-    else{
-      res.render('profile', {
-        p: p
-      });
-    }
+		if (p) {
+			options.p = p
+      Watch.find({'criador': options.p._id}, null, {sort: '-id'}, function(err, watch){
+
+        if (watch) {
+          options.watch = watch
+        }
+        else {
+          options.watch = null
+        }
+        res.render('profile', options);
+      }).limit();
+		}
   });
 };
