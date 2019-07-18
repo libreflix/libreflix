d925f79d5e1ffcd624e3399ba6beed0915e87879
Guilmour Rossi <me@guilmour.org>
2019-07-17 22:09:32 -0300

    Random exibition of the films

--- controllers/home.js
+++ controllers/home.js
@@ -8,8 +8,47 @@ var env = new nunjucks.Environment();
  * GET /
  */
 
+
 exports.index = function(req, res) {
-  Watch.find({}, null, {sort: '-updatedAt'}, function(err, watch){
+  /*
+  * Crazy code just to make a fresh look on the homepage everytime.
+  * Very ugly solution, just temporary.
+  */
+
+  function random(low, high) {
+    return Math.random() * (high - low) + low
+  }
+
+  random = random(0, 7)
+
+  console.log(random);
+
+  if (random > 0 && random < 1 ) {
+    property = '-updatedAt'
+  }
+  if (random > 1 && random < 2 ) {
+    property = 'updatedAt'
+  }
+  if (random > 2 && random < 3 ) {
+    property = 'year'
+  }
+  if (random > 3 && random < 4 ) {
+    property = '-year'
+  }
+  if (random > 4 && random < 5 ) {
+    property = 'id'
+  }
+  if (random > 5 && random < 6 ) {
+    property = '-id'
+  }
+  if (random > 6 && random < 7 ) {
+    property = 'title'
+  }
+  if (random > 7 && random < 8 ) {
+    property = '-title'
+  }
+
+  Watch.find({}, null, {sort: property}, function(err, watch){
     if(err){
       console.log(err);
     } else {
