96fea59c69b24f767e31930e9964ef5560bb4d84
Guilmour Rossi <me@guilmour.org>
2024-07-04 15:13:07 -0300

    Converting Models

--- models/Reference.js
+++ models/Reference.js
@@ -1,20 +1,19 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var schemaOptions = {
-  timestamps: true,
-  toJSON: {
-    virtuals: true
-  }
+const schemaOptions = {
+    timestamps: true,
+    toJSON: {
+        virtuals: true
+    }
 };
 
-var refereceSchema = new mongoose.Schema({
-  attachedToWatch: { type : mongoose.Schema.ObjectId, ref : 'Watch' },
-  creator: { type : mongoose.Schema.ObjectId, ref : 'User' },
-  url: String,
-  title: String,
+const refereceSchema = new mongoose.Schema({
+    attachedToWatch: {type: mongoose.Schema.ObjectId, ref: 'Watch'},
+    creator: {type: mongoose.Schema.ObjectId, ref: 'User'},
+    url: String,
+    title: String,
 }, schemaOptions);
 
-
-var Reference = mongoose.model('Reference', refereceSchema);
+const Reference = mongoose.model('Reference', refereceSchema);
 
 module.exports = Reference;
