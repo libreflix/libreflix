d3134956e62a97c55c1f11111b83cba932ce05a6
Guilmour Rossi <me@guilmour.org>
2024-07-04 18:53:06 -0300

    [TECH DEBT] Fixing minor bugs and tech debts for version 2.2.0 (#24)

--- models/Comment.js
+++ models/Comment.js
@@ -1,22 +1,21 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var schemaOptions = {
-  timestamps: true,
-  toJSON: {
-    virtuals: true
-  }
+const schemaOptions = {
+    timestamps: true,
+    toJSON: {
+        virtuals: true
+    }
 };
 
-var commentSchema = new mongoose.Schema({
-  attachedToUser: { type : mongoose.Schema.ObjectId, ref : 'User' },
-  attachedToWatch: { type : mongoose.Schema.ObjectId, ref : 'Watch' },
-  creator: { type : mongoose.Schema.ObjectId, ref : 'User' },
-  date: Date,
-  type: String, //public, admin, deleted
-  body: String,
+const commentSchema = new mongoose.Schema({
+    attachedToUser: {type: mongoose.Schema.ObjectId, ref: 'User'},
+    attachedToWatch: {type: mongoose.Schema.ObjectId, ref: 'Watch'},
+    creator: {type: mongoose.Schema.ObjectId, ref: 'User'},
+    date: Date,
+    type: String, //public, admin, deleted
+    body: String,
 }, schemaOptions);
 
-
-var Comment = mongoose.model('Comment', commentSchema);
+const Comment = mongoose.model('Comment', commentSchema);
 
 module.exports = Comment;
