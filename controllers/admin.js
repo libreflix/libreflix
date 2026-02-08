5f6a5ccc08a08cf75c07376439df5fde26539150
Enieber Cunha <enieber.silva@kmee.com.br>
2026-02-08 04:54:54 -0300

    fix: update version and use nginx

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,70 +1,70 @@
-var User = require('../models/User');
-let Watch = require('../models/Watch');
-let Category = require('../models/Category');
-var mongoose = require('mongoose');
+var User = require("../models/User");
+let Watch = require("../models/Watch");
+let Category = require("../models/Category");
+var mongoose = require("mongoose");
+const { body, validationResult } = require("express-validator");
 
 exports.listWatches = (req, res) => {
-
-  Watch.find({}, null, { sort: '-id' }, function (err, watch) {
+  Watch.find({}, null, { sort: "-id" }, function (err, watch) {
     if (err) {
       console.log(err);
     } else {
-      res.render('admin/list-watches', {
-        title: 'List watches',
-        watch: watch
+      res.render("admin/list-watches", {
+        title: "List watches",
+        watch: watch,
       });
     }
-  }).populate('criador');
-
-}
+  }).populate("criador");
+};
 
 exports.listCategoriesGet = (req, res) => {
-  Category.find({}, null, { sort: '-id' }, function (err, categories) {
+  Category.find({}, null, { sort: "-id" }, function (err, categories) {
     if (err) {
       console.log(err);
     } else {
-      res.render('admin/list-categories', {
-        title: 'List Categories',
-        categories: categories
+      res.render("admin/list-categories", {
+        title: "List Categories",
+        categories: categories,
       });
     }
-  }).populate('criador');
-}
+  }).populate("criador");
+};
 
 /**
  * PUT /admin/list-categories
  * Add or update a new category.
  */
-exports.listCategoriesPut = function(req, res, next) {
+exports.listCategoriesPut = async function (req, res, next) {
+  await body("title", "O campo título precisa ser preenchido.").run(req);
+  await body("nid", "O campo nid precisa ser preenchido.").run(req);
 
-    req.assert('title', 'O campo título precisa ser preenchido.').notEmpty();
-    req.assert('nid', 'O campo nid precisa ser preenchido.').notEmpty();
-
-  var errors = req.validationErrors();
+  var errors = !validationResult(req).isEmpty();
 
   if (errors) {
-    req.flash('error', errors);
-    return res.redirect('/admin/list-categories');
+    req.flash("error", errors);
+    return res.redirect("/admin/list-categories");
   }
 
-  Category.findOne({ nid: req.body.nid }, function(err, category) {
-    	if (category) {
-      	req.flash('error', { msg: 'O Number Id inserido já existe. Tente outro.' });
-      	return res.redirect('/admin/list-categories');
-    	}
-      // Para salvar no BD
-   	category = new Category({
+  Category.findOne({ nid: req.body.nid }, function (err, category) {
+    if (category) {
+      req.flash("error", {
+        msg: "O Number Id inserido já existe. Tente outro.",
+      });
+      return res.redirect("/admin/list-categories");
+    }
+    // Para salvar no BD
+    category = new Category({
       criador: req.user.id,
       nid: req.body.nid,
       format: req.body.format,
       preposition: req.body.preposition,
-      title: req.body.title
+      title: req.body.title,
     });
 
-    category.save(function(err) {
+    category.save(function (err) {
       //req.logIn(campanha, function(err) {
-        req.flash('success', { msg: 'Categoria inserida com sucesso.' });
-        res.redirect('/admin/list-categories');
+      req.flash("success", { msg: "Categoria inserida com sucesso." });
+      res.redirect("/admin/list-categories");
       //});
     });
   });
